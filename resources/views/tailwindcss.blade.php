4c807356154162f799c64c466cb82e63b0fc2c19
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-02-29 10:58:45 +0800

    TailwindCSS: Flex, Height & Width, Object

--- resources/views/tailwindcss.blade.php
+++ resources/views/tailwindcss.blade.php
@@ -1,5 +1,13 @@
 <x-guest-layout>
     <div class=" bg-slate-700 w-screen h-screen sm:py-12 lg:py-4">
         <p class="text-7xl text-center cursor-pointer text-slate-100 hover:text-pink-700">Hello World</p>
+
+        <div class="flex justify-center my-2">
+            <div class=" h-96 w-96 border border-slate-300  p-4">
+                <img src="{{ url('images/manja-vitolic-gKXKBY-C-Dk-unsplash.jpg') }}" class="object-cover">
+            </div>
+        </div>
+
     </div>
+
 </x-guest-layout>
